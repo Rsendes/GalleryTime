317a8bd64472a8bce34c39a8930867a15d8f4124
Filipe Resendes <filiperesendes@proton.me>
2024-12-29 12:28:14 -0100

    Added loops to display all images

--- gallery_time.py
+++ gallery_time.py
@@ -6,6 +6,21 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import GLib, Gtk, Gdk
 
+month_names = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
+
 class GalleryTime(Gtk.Application):
     def __init__(self, base_path):
         print("Program Start\n")
@@ -50,6 +65,15 @@ class GalleryTime(Gtk.Application):
                 self.images_by_date[year][month] = []
             self.images_by_date[year][month].append(image)
 
+    def load_image(self, image_path):
+        try:
+            image_widget = Gtk.Image.new_from_file(image_path)
+            image_widget.get_style_context().add_class("image")
+            return image_widget
+        except Exception as e:
+            print(f"Failed to load image {image_path}: {e}")
+            return None
+
     def do_activate(self):
         window = Gtk.ApplicationWindow(application=self)
         window.set_title("Gallery Time")
@@ -69,40 +93,13 @@ class GalleryTime(Gtk.Application):
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
 
-
         #Scroll and main container
         scroll = Gtk.ScrolledWindow()
         window.set_child(scroll)
 
         main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         scroll.set_child(main_box)
-
-        year = 2006
-        year_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-        year_label = Gtk.Label(label=str(year))
-        year_label.set_markup(f"<b>{year}</b>")
-        year_box.append(year_label)
-
-        month = 8
-        month_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=1)
-        month_label = Gtk.Label(label=f"{month:02d}")
-        month_label.set_markup(f"<i>{month:02d}</i>")
-        month_box.append(month_label)
-
-        image_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
-        
-        for i in range(9): 
-            image = self.images[i]
-            relative_path = self.get_relative_path(image)
-            image_path = os.path.join(self.base_path, relative_path ,image)
-            image_widget = Gtk.Image.new_from_file(image_path)
-            image_widget.get_style_context().add_class("image") 
-            image_box.append(image_widget)
-        month_box.append(image_box)
-        year_box.append(month_box)
-        main_box.append(year_box)
-
-        """
+ 
         for year, months in self.images_by_date.items():
             year_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
             year_label = Gtk.Label(label=str(year))
@@ -110,20 +107,26 @@ class GalleryTime(Gtk.Application):
             year_box.append(year_label)
 
             for month, images in months.items():
+                month_name = month_names[month] 
                 month_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
-                month_label = Gtk.Label(label=f"{month:02d}")
-                month_label.set_markup(f"<i>{month:02d}</i>")
+                month_label = Gtk.Label(label=f"{month_name}")
+                month_label.set_markup(f"<i>{month_name}</i>")
                 month_box.append(month_label)
 
-                for image in images:
-                    image_relative_path = self.get_relative_path(image)
-                    image_path = os.path.join(self.base_path, image)
-                    image_widget = Gtk.Image.new_from_file(image_path)
-                    month_box.append(image_widget)
+                image_box = Gtk.FlowBox()
+                image_box.set_max_children_per_line(3)
+                image_box.set_selection_mode(Gtk.SelectionMode.NONE)
 
+                for image in images:
+                    relative_path = self.get_relative_path(image)
+                    image_path = os.path.join(self.base_path, relative_path ,image)
+                    image_widget = self.load_image(image_path)
+                        if image_widget:
+                            image_box.insert(image_widget, -1)
+
+                month_box.append(image_box)
                 year_box.append(month_box)
             main_box.append(year_box)
-        """
         window.present()
 
 
