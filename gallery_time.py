10ecb24d57ac9954933b5074816011fa3a1c4437
Filipe Resendes <filiperesendes@proton.me>
2025-02-16 23:31:32 +0000

    Added thumbnails to improve perforrmance

--- gallery_time.py
+++ gallery_time.py
@@ -1,10 +1,12 @@
 import os
 import gi
+import numpy as np
+from PIL import Image
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import GLib, Gtk, Gdk
 
-month_names = {
+MONTH_NAMES = {
     1: 'January',
     2: 'February',
     3: 'March',
@@ -19,15 +21,23 @@ month_names = {
     12: 'December'
 }
 
+BASE_PATH = "/home/filipe/Pictures/Fotos"
+THUMBNAILS_PATH = BASE_PATH + "/Thumbnails"
+IGNORE_PATH = "Thumbnails"
+
+THUMBNAIL_SIZE = (300, 300)
+
 class Gallery():
     def __init__(self):
         print("Program Start")
-        self.base_path = "/home/filipe/Pictures/Fotos"
         self.images = []
+        self.thumbnails = []
         self.load_images()
+        self.load_thumbnails()
+        self.create_thumbnails()
 
     def is_valid(self, file):
-        return file[0:2] == "20" and file[-1] != "4"
+        return file[0:2] == "20" and file[-1] != "4" and file[-3] != "3"
 
     def get_year(self, file):
         return int(file[:4])
@@ -37,17 +47,45 @@ class Gallery():
 
     def load_images(self):
         print("Loading Images\n")
-        for _, _, files in  os.walk(self.base_path):
+        for root, dirs, files in  os.walk(BASE_PATH):
+            if os.path.basename(root) == IGNORE_PATH:
+                print("Path Ignored")
+                continue  
             for file in files:
                 if self.is_valid(file): 
                     self.images.append(file)
         self.images.sort()
 
+    def load_thumbnails(self):
+        print("Loading Thumbnails\n")
+        for _, _, files in  os.walk(THUMBNAILS_PATH):
+            for file in files:
+                self.thumbnails.append(file)
+        self.thumbnails.sort(reverse=True)
+
+    def create_thumbnails(self):
+        if len(self.images) > len(self.thumbnails):
+            print("Creating Thumbnails")
+            for image in self.images:
+                if image not in self.thumbnails:
+                    self.create_thumbnail(image)
+
+    def create_thumbnail(self, image):
+        print("Creating thumbnail " + image)
+        full_path = self.get_full_path(image)
+        img = Image.open(full_path)
+        resized_img = img.resize(THUMBNAIL_SIZE)
+        thumbnail_path = os.path.join(THUMBNAILS_PATH, image)
+        resized_img.save(thumbnail_path)
+
     def get_full_path(self, file):
         year = file[2:4]
         month = file[5:7]
         relative_path = year + '/' + year + month + '/'
-        return os.path.join(self.base_path, relative_path, file)
+        return os.path.join(BASE_PATH, relative_path, file)
+
+    def get_thumbnail_path(self, file):
+        return os.path.join(THUMBNAILS_PATH, file)
 
 class App(Gtk.Application):
     def __init__(self):
@@ -83,24 +121,25 @@ class MainWindow(Gtk.ApplicationWindow):
         scroll.set_child(main_box)
 
         # First Year and Month
-        current_year = gallery.get_year(gallery.images[0]) 
-        current_month = gallery.get_month(gallery.images[0])
-        image_box, _ = self.new_year_month(main_box, None, gallery.images[0])
+        current_year = gallery.get_year(gallery.thumbnails[0]) 
+        current_month = gallery.get_month(gallery.thumbnails[0])
+        image_box, year_box = self.new_year_month(main_box, None, gallery.thumbnails[0])
 
-        for image in gallery.images:
+        for image in gallery.thumbnails:
             image_year = gallery.get_year(image)
             image_month = gallery.get_month(image)
-            
+
             # The year changes
             if current_year != image_year:
                 current_year = image_year
                 image_box, year_box = self.new_year_month(main_box, None, image) 
+            
             # Only the month changes
             elif current_month != image_month:
                 current_month = image_month
                 image_box, _ = self.new_year_month(main_box, year_box, image)
 
-            image_path = gallery.get_full_path(image)
+            image_path = gallery.get_thumbnail_path(image)
             image_widget = Gtk.Image.new_from_file(image_path)
             image_widget.get_style_context().add_class("image")
             image_box.insert(image_widget, -1)
@@ -118,7 +157,7 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def create_image_box(self):
         image_box = Gtk.FlowBox()
-        image_box.set_max_children_per_line(3)
+        image_box.set_max_children_per_line(5)
         image_box.set_selection_mode(Gtk.SelectionMode.NONE)
         return image_box 
 
@@ -130,7 +169,7 @@ class MainWindow(Gtk.ApplicationWindow):
         return year_box
 
     def display_month(self, month):
-        month_name = month_names[month] 
+        month_name = MONTH_NAMES[month] 
         month_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
         month_label = Gtk.Label(label=f"{month_name}")
         month_label.set_markup(f"<i>{month_name}</i>")
